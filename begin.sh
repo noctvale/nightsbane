048f035dd03d7cd32e0bf8f0df42db8c34eec01b
noctvale <noctvale@outlook.com>
2025-06-08 13:50:46 +0800

    change setup.sh to begin.sh

--- begin.sh
+++ begin.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+
 sudo apt update && sudo apt upgrade -y
 
 sudo apt install software-properties-common build-essential vim tmux git \
-curl unzip gnupg ca-certificates lsb-release lynx
+curl unzip gnupg ca-certificates lsb-release lynx stow xautolock xss-lock \
+pavucontrol
+
+stow .
 
