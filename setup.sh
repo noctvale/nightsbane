6775f0fe7dee75a94af6b9d26cef712750c0d128
noctvale <noctvale@outlook.com>
2025-06-08 13:47:14 +0800

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
-apt update && sudo apt upgrade -y
-apt update
-apt install software-properties-common
-add-apt-repository --yes --update ppa:ansible/ansible
+#!/bin/bash
+
+sudo apt update && sudo apt upgrade -y
+
+sudo apt install software-properties-common build-essential vim tmux git \
+curl unzip gnupg ca-certificates lsb-release lynx
+
