3d2a5f33e4c1b14d5ffcb11d6ae72926eb664028
noctvale <noctvale@outlook.com>
2025-06-08 14:00:53 +0800

    add packages to installs

--- begin.sh
+++ begin.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 
-sudo apt update && sudo apt upgrade -y
+sudo add-apt-repository ppa:neovim-ppa/unstable 
 
-sudo apt install software-properties-common build-essential vim tmux git \
+
+sudo apt-get update && sudo apt-get upgrade -y
+
+sudo apt-get install software-properties-common build-essential vim tmux git \
 curl unzip gnupg ca-certificates lsb-release lynx stow xautolock xss-lock \
-pavucontrol
+pavucontrol neovim python3 python3-dev python3-pip
 
 stow .
+
 
