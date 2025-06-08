0aef4af1ae0e72d49cb2bfaa8fd82ebc6c59cf0d
noctvale <noctvale@outlook.com>
2025-06-08 15:38:57 +0800

    simplify neovim config

--- .config/nvim/lua/jalusw/packer.lua
+++ .config/nvim/lua/jalusw/packer.lua
@@ -18,11 +18,6 @@ return require("packer").startup(function(use)
   })
 
   use({
-    "nvim-treesitter/playground",
-    requires = { "nvim-treesitter/nvim-treesitter" },
-  })
-
-  use({
     "VonHeikemen/lsp-zero.nvim",
     branch = "v4.x",
     requires = {
@@ -37,38 +32,9 @@ return require("packer").startup(function(use)
   })
 
   use({
-    "mfussenegger/nvim-dap",
-    requires = {
-      { "leoluz/nvim-dap-go" },
-      { "williamboman/mason.nvim" },
-      { "theHamsta/nvim-dap-virtual-text" },
-      { "rcarriga/nvim-dap-ui" },
-    },
-    dependencies = {
-      "rcarriga/nvim-dap-ui",
-    },
-  })
-
-  use({ "rcarriga/nvim-dap-ui", requires = { "mfussenegger/nvim-dap", "nvim-neotest/nvim-nio" } })
-  use({ "mxsdev/nvim-dap-vscode-js" })
-  use({
-    "microsoft/vscode-js-debug",
-    opt = true,
-    run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out",
-  })
-  use({
     "cbochs/grapple.nvim",
     requires = { "nvim-tree/nvim-web-devicons" },
   })
-
-  use({
-    "windwp/nvim-autopairs",
-    event = "InsertEnter",
-    config = function()
-      require("nvim-autopairs").setup({})
-    end,
-  })
-
   use({
     "ThePrimeagen/refactoring.nvim",
     requires = {
@@ -76,41 +42,9 @@ return require("packer").startup(function(use)
       { "nvim-treesitter/nvim-treesitter" },
     },
   })
-
   use({
     "stevearc/oil.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
   })
-
-  use({
-    "iamcco/markdown-preview.nvim",
-    run = function()
-      vim.fn["mkdp#util#install"]()
-    end,
-  })
-
-  use({
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    requires = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim",              -- Optional image support in preview window: See `# Preview Mode` for more information
-    },
-  })
-
-  use("nvim-telescope/telescope-symbols.nvim")
-
-  use("rose-pine/neovim")
-
-  use("lewis6991/gitsigns.nvim")
-  use("stevearc/conform.nvim")
-  use("hedyhli/outline.nvim")
-  use("tpope/vim-fugitive")
-  use("tpope/vim-surround")
-  use("folke/trouble.nvim")
   use("folke/flash.nvim")
-  use("kevinhwang91/nvim-bqf")
-  use("leoluz/nvim-dap-go")
 end)
--- begin.sh
+++ begin.sh
@@ -2,14 +2,15 @@
 
 
 sudo add-apt-repository ppa:neovim-ppa/unstable 
-
-
 sudo apt-get update && sudo apt-get upgrade -y
-
 sudo apt-get install software-properties-common build-essential vim tmux git \
+
 curl unzip gnupg ca-certificates lsb-release lynx stow xautolock xss-lock \
-pavucontrol neovim python3 python3-dev python3-pip
+
+pavucontrol neovim python3 python3-dev python3-pip lua5.1
+
+git clone --depth 1 https://github.com/wbthomason/packer.nvim\
+ ~/.local/share/nvim/site/pack/packer/start/packer.nvim
 
 stow .
-
 
